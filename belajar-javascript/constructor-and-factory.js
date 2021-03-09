ba2b99eaea6c2171fe00d4c7e831e36c7caab914
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-03-09 22:31:29 +0700

    Refactor: implementing eslint

--- belajar-javascript/constructor-and-factory.js
+++ belajar-javascript/constructor-and-factory.js
@@ -1,20 +1,23 @@
-// Factory function 
+// Factory function
 function createCircle(radius) {
-    return {
-        radius,
-        draw: function() {
-            console.log('draw');
-        }
-    }
+  return {
+    radius,
+    draw: () => {
+      console.log('draw');
+    },
+  };
 }
+
 const circle = createCircle(1);
 circle.draw();
 
 // Constructor Function
 function Circle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
+  this.radius = radius;
+  this.draw = function () {
+    console.log('draw');
+  };
 }
+
 const another = new Circle(1);
+another.draw();
--- belajar-javascript/encapsulation.js
+++ belajar-javascript/encapsulation.js
@@ -1,18 +1,22 @@
-let baseSalary = 30000;
-let overtime = 10;
-let rate = 20;
+/* eslint-disable object-shorthand */
 
-function getWage(baseSalary, overtime, rate) {
-    return baseSalary + (overtime * rate);
-}
-
-let employee = {
-    baseSalary: 30000,
-    overtime: 10,
-    rate: 20,
-    getWage: function() {
-        return this.baseSalary + (this.overtime * this.rate);
-    },    
-}
+const employee = {
+  baseSalary: 30000,
+  overtime: 10,
+  rate: 20,
+  getWage: function () {
+    return this.baseSalary + (this.overtime * this.rate);
+  },
+};
 
 console.log(employee.getWage());
+
+function getWage(baseSalary, overtime, rate) {
+  return baseSalary + (overtime * rate);
+}
+
+const baseSalary = 30000;
+const overtime = 10;
+const rate = 20;
+
+getWage(baseSalary, overtime, rate);
