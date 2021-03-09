3033aea889a3338ae9fe0aa7eb116423493e8d51
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-03-09 22:25:42 +0700

    menerapkan best practice pada Function Declaration

--- belajar-javascript/object.js
+++ belajar-javascript/object.js
@@ -14,20 +14,28 @@ mahasiswa.makan(2);
 console.log(mahasiswa.energy);
 
 // 2. Function Declaration
+const methodMahasiswa = {
+  makan: function (porsi) {
+    this.energy += porsi;
+    console.log(`Halo ${this.nama}, selamat makan`);
+  },
+
+  main: function (jam) {
+    this.energy -= jam;
+    console.log(`Halo ${this.nama}, selamat bermain`);
+  },
+};
+
 function Mahasiswa(nama, energy) {
   const mhs = {};
   mhs.nama = nama;
   mhs.energy = energy;
 
-  mhs.makan = function (porsi) {
-    this.energy += porsi;
-    console.log(`Halo ${this.nama}, selamat makan`);
-  };
-
-  mhs.main = function (jam) {
-    this.energy -= jam;
-    console.log(`Halo ${this.nama}, selamat bermain`);
-  };
+  // method 'makan' dan 'minum' diletakkan di object baru agar tidak di buat ulang di memori
+  // sehingga ketika kita membuat banyak objek, tidak banyak function yg dibuat pula
+  // function 'makan' dan 'minum' tinggal reference ke 'methodMahasiswa' saja yg telah ada di memory
+  mhs.makan = methodMahasiswa.makan;
+  mhs.main = methodMahasiswa.main;
 
   return mhs;
 }
