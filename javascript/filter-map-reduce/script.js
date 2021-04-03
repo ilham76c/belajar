47af82fe7c0c471e321cec4c680d6cfddb444113
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-04-03 15:26:42 +0700

    latihan Filter, Map & Reduce

---
+++ javascript/filter-map-reduce/script.js
@@ -0,0 +1,53 @@
+// ambil semua element video
+/**
+ * Array.from() digunakan untuk mengubah NodeList menjadi Array
+ * Karena return dari document.querySelectorAll() adalah NodeList
+ */
+const videos = Array.from(document.querySelectorAll('[data-duration]'));
+
+
+// pilih hanya yang 'JAVASCRIPT LANJUTAN'
+let jsLanjut = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN'))
+
+
+// ambil durasi masing-masing video
+  .map(item => item.dataset.duration)
+
+
+// ubah durasi menjadi float, ubah menit menjadi detik
+  .map(waktu => {
+    // 10:30 --> [10, 30] split
+    const parts =  waktu.split(':').map(part => parseFloat(part));
+    return (parts[0] * 60) + parts[1];
+  })
+
+
+// jumlahkan semua detik
+  .reduce((total, detik) => total + detik);
+
+
+// ubah formatnya jadi jam menit detik
+/**
+ * Math.floor() --> pembulatan ke bawah
+ * Math.ceil()  --> pembulatan ke atas
+ * Math.round() --> pembulatan ke yang paling terdekat
+ */
+const jam = Math.floor(jsLanjut / 3600);
+jsLanjut = jsLanjut - jam * 3600;
+
+const menit = Math.floor(jsLanjut / 60);
+
+const detik = jsLanjut - menit * 60;
+
+
+// simpan di DOM
+const pDurasi = document.querySelector('.total-durasi');
+pDurasi.textContent = `${jam} Jam, ${menit} Menit, ${detik} Detik`;
+
+const jmlVideo = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN')).length;
+const pJmlVideo = document.querySelector('.jumlah-video');
+pJmlVideo.textContent = `${jmlVideo} Video`;
+
+
+
+//console.log(jsLanjut);
