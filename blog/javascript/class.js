304b96c4e4a04da943ae9b3ee936cc0c6f7766e4
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-09-07 23:21:09 +0700

    add class expressions

--- blog/javascript/class.js
+++ blog/javascript/class.js
@@ -13,3 +13,31 @@ class Rectangle {
 
 const rectangleInstance = new Rectangle(10,15);
 console.log(rectangleInstance.height);
+
+// Class expressions
+/**
+ * Class expressions bisa diberi nama ataupun tidak, 
+ * nama yang diberikan pada Class expression bersifat lokal pada class body.
+ * nama class bisa diakses melalui property name
+ */
+
+// unnamed
+const Luas1 = class {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+console.log(Luas1.name);
+// output: "Rectangle"
+
+// named
+const Luas2 = class Luas {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+console.log(Luas2.name);
+// output: "Rectangle2"
+  
