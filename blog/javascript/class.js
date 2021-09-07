ff13383bedbe8062d0861bb4ba50e221b39f9572
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-09-08 00:44:43 +0700

    Static methods and properties

--- blog/javascript/class.js
+++ blog/javascript/class.js
@@ -73,16 +73,43 @@ console.log(square.area); // 100
 // Generator methods
 class Polygon {
     constructor(...sides) {
-      this.sides = sides;
+        this.sides = sides;
     }
     // Method
     *getSides() {
-      for(const side of this.sides){
-        yield side;
-      }
+        for(const side of this.sides){
+            yield side;
+        }
     }
-  }
+}
   
-  const pentagon = new Polygon(1,2,3,4,5);
+const pentagon = new Polygon(1,2,3,4,5);
   
-  console.log([...pentagon.getSides()]); // [1,2,3,4,5]
+console.log([...pentagon.getSides()]); // [1,2,3,4,5]
+
+// Static methods and properties
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+  
+    static displayName = "Point";
+    static distance(a, b) {
+        const dx = a.x - b.x;
+        const dy = a.y - b.y;
+    
+        return Math.hypot(dx, dy);
+    }
+}
+  
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
+p1.displayName; // undefined
+p1.distance;    // undefined
+p2.displayName; // undefined
+p2.distance;    // undefined
+
+console.log(Point.displayName);      // "Point"
+console.log(Point.distance(p1, p2)); // 7.0710678118654755
+  
