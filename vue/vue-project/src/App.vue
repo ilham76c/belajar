da5ee95e8c8234ee559e3be0d72059b0fb6fc012
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-09-12 22:23:21 +0700

    vue data & methods

--- vue/vue-project/src/App.vue
+++ vue/vue-project/src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  
+  <h2>Nama: {{ nama }}</h2>
+  <button v-on:click="changeName">Change Name</button>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  components: {
-    
-  }
+  data() {
+    return {
+      nama: 'Moh. Ilham B.',
+    };
+  },
+  methods: {
+    changeName() {
+      this.nama = 'Okee';
+    },
+  },
 }
 </script>
 
