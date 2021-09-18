b8f11f7eafd6cd3697c3a710d5eb21404a80d70a
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-09-18 23:58:45 +0700

    create function to get data customers and contacts using postgresql join

--- CobaSequelize/index.js
+++ CobaSequelize/index.js
@@ -9,10 +9,24 @@ const app = {
     } catch (error) {
       console.error(error);
     }
-  }
+  },
+  getCustomers: async () => {
+    try {
+      return await db.query(`
+        SELECT * FROM customers
+          LEFT JOIN contacts
+          ON 
+            customers.customer_id = contacts.customer_id
+          WHERE 
+            contacts.customer_id IS NOT NULL;
+      `, { type: QueryTypes.SELECT });     
+    } catch (error) {
+      console.error(error);
+    }
+  },
 }
 
-app.getContacts()
+app.getCustomers()
   .then(result => {
     console.log(result);
   })
