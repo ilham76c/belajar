ad8edfd0fef5ff07de155459c41707febc5a29d0
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-12-27 22:39:08 +0700

    membuat file khusus yang memanfaatkan class Database di dalamnya

---
+++ CobaSequelize/use-singleton-pattern.js
@@ -0,0 +1,24 @@
+/* import Sequery QueryTypes untuk melakukan query ke database */
+const { QueryTypes } = require('sequelize');
+
+/* import class Database */
+const { Database } = require('./db-singleton-pattern');
+
+/* panggil function getInstance() dari class Database untuk mendapatkan instance */
+const db = Database.getInstance();
+
+/* contoh function untuk melakukan query ke database menggunakan instance yang di dapatkan dari class Database */
+async function getData() {
+    return await db.query("SELECT * FROM contacts", { type: QueryTypes.SELECT });
+}
+
+/* panggil function getData() untuk mengeksekusi query */
+getData()
+    .then(result => {
+        /* jika berhasil tampilkan hasil dari query */
+        console.log(result);
+    })
+    .catch(error => {
+        /* jika gagal tampilkan log error */
+        console.error(error);
+    });
