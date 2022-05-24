68c395337c1b49ec0e6eec05bd6e14787177296d
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-05-24 22:52:55 +0700

    angular variable

--- angular/hello-world/src/app/hello-world.component.ts
+++ angular/hello-world/src/app/hello-world.component.ts
@@ -2,13 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'hello-world',
-  template: `
-    <h2>Hello World</h2>
-    <p>component angular pertama</p>
-  `,
+  templateUrl: './hello-world.component.html',
   styles: [`h2, p { font-family: sans-serif; margin: 10px; color: blue; }`]
 })
 
 export class HelloWorldComponent {
-
+  judul = 'Hello World.';
+  gambar = 'assets/angular.png';
+  aktif = true;
 }
