6f7f289e525b83465a1890eb5586e3428acdcee9
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-06-05 02:02:18 +0700

    golang math operations

---
+++ golang/MathOperation.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Math Operation
+	var result = 15 + 15
+	fmt.Println(result)
+
+	// Augmented Assignments
+	var count = 1
+	count += 5
+	fmt.Println(count)
+
+	// Unary Operator
+	var i = 1
+	i++
+	fmt.Println(i)
+
+	var negatif = -100
+	var positif = +100
+	fmt.Println(negatif)
+	fmt.Println(positif)
+}
