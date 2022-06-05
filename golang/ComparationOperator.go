da18914a593bd11b54f6bcc6b516aee91fcdae5f
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-06-06 00:53:26 +0700

    golang comparation operator

---
+++ golang/ComparationOperator.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "ilham"
+	var name2 = "tes"
+
+	var result1 bool = name1 == name2
+	fmt.Println(result1)
+
+	var angka1 = 1
+	var angka2 = 5
+
+	var result2 bool = angka1 < angka2
+	fmt.Println(result2)
+}
