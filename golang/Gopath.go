8efd8930ce736fa264de950ebf72b39354446234
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-06-14 22:22:35 +0700

    golang gopath

---
+++ golang/Gopath.go
@@ -0,0 +1,8 @@
+// GOPATH adalah environment variable yang berisikan lokasi tempat kita menyimpan project dan libray GO-Lang
+// GOPATH wajib dibuat ketika kita mulai membuat aplikasi lebih dari satu file atau butuh menggunakan library dari luar
+
+// Setting GOPATH di Linux atau MAC
+
+/* Add to .bashrc or .zshrc or .profile */
+/* ketika membuat project dan men-download library maka akan masuk ke folder "FolderProjectGoLang" */
+export GOPATH="/home/ilham76c/Documents/FolderProjectGoLang"
