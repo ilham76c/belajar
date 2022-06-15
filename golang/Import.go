891ea5c107f98b5eb4a9cdd65197816389904907
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-06-15 09:52:02 +0700

    rename folder

---
+++ golang/Import.go
@@ -0,0 +1,7 @@
+package main
+
+import "golang/helper"
+
+func main() {
+	helper.sayHello("ilham")
+}
---
+++ golang/helper/Package.go
@@ -0,0 +1,5 @@
+package helper
+
+func sayHello(name string) string {
+	return "Hello " + name
+}
