c8828d2bb09657d29269116f57efbd4f215bdef2
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-06-18 00:27:24 +0700

    golang package math

---
+++ golang/src/golang/PackageMath.go
@@ -0,0 +1,13 @@
+package main 
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Round(1.7))
+	fmt.Println(math.Round(1.3))
+	fmt.Println(math.Max(3, 12))
+	fmt.Println(math.Min(8, 4))
+}
