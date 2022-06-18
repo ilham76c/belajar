38ce5f85d96100fa114817aa1583a752cf0640ca
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-06-19 00:25:17 +0700

    menampilkan done dan undone todos

--- todo-apps/simple-todo-app/js/script.js
+++ todo-apps/simple-todo-app/js/script.js
@@ -10,13 +10,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
   document.addEventListener(RENDER_EVENT, function () {
     console.log(todos);
+
     const uncompletedTODOList = document.getElementById('todos');
     uncompletedTODOList.innerHTML = '';
+
+    const completedTODOList = document.getElementById('completed-todos');
+    completedTODOList.innerHTML = '';
   
     for (const todoItem of todos) {
       const todoElement = makeTodo(todoItem);
       if (!todoItem.isCompleted) {
         uncompletedTODOList.append(todoElement);
+      } else {
+        completedTODOList.append(todoElement);
       }
     }
   });
