2303adf1463b8df481f7fb069a8e78ebc78bdd3d
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-07-06 23:59:24 +0700

    nodejs path

---
+++ node.js/6-path.mjs
@@ -0,0 +1,8 @@
+import path from 'path';
+
+const file = '5-os.mjs';
+
+console.info(path.sep)
+console.info(path.dirname(file));
+console.info(path.basename(file));
+console.info(path.extname(file));
