3900d0cc6ff2bd76c7c021097349301d39172694
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-07-27 23:51:45 +0700

    add style & group file in folder

--- react-contacts-app/src/index.js
+++ react-contacts-app/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import ContactApp from './ContactApp';
+import ContactApp from './components/ContactApp';
+
+// import css cara 2
+import './styles/style.css';
 
 // const element = <h1>Hello, world!</h1>;
  
