a4d6bad3e9d44bde6827dd962487db20798ff513
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-08-02 00:40:05 +0700

    react controlled component case form input

--- react/react-controlled-component/src/index.js
+++ react/react-controlled-component/src/index.js
@@ -1,17 +1,84 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+ 
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+ 
+    // inisialisasi state
+    this.state = {
+      name: '',
+      email: '',
+      gender: 'Man'
+    };
+ 
+    // binding this context to event handler
+    this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+    this.onEmailChangeEventHandler = this.onEmailChangeEventHandler.bind(this);
+    this.onGenderChangeEventHandler = this.onGenderChangeEventHandler.bind(this);
+    this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+  }
+ 
+  onNameChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        name: event.target.value
+      };
+    });
+  }
+ 
+  onEmailChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        email: event.target.value
+      };
+    });
+  }
+ 
+  onGenderChangeEventHandler(event) {
+    this.setState((prevState) => {
+      return {
+        gender: event.target.value
+      };
+    });
+  }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  onSubmitEventHandler(event) {
+    // menghentikan aksi default dari tombol submit
+    event.preventDefault();
+  
+    const message = `
+      Name: ${this.state.name}
+      Email: ${this.state.email}
+      Gender: ${this.state.gender}
+    `;
+  
+    alert(message);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  render() {
+    return (
+      <div>
+        <h1> Register Form</h1>
+        <form onSubmit={this.onSubmitEventHandler}>
+          <label htmlFor="name">Name: </label>
+          <input id="name" type="text" value={this.state.name} onChange={this.onNameChangeEventHandler} />
+          <br />
+          <label htmlFor="email">Email: </label>
+          <input id="email" type="text" value={this.state.email} onChange={this.onEmailChangeEventHandler} />
+          <br />
+          <label htmlFor="gender">Gender: </label>
+          <select id="gender" value={this.state.gender} onChange={this.onGenderChangeEventHandler}>
+            <option value="Man">Man</option>
+            <option value="Woman">Woman</option>
+          </select>
+          <br />
+          <button type="submit">submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+ 
+const root = createRoot(document.getElementById('root'));
+root.render(<MyForm />);
