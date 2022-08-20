45c55247d7151a71391cf71532dac1fbe603f21c
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-08-20 23:25:13 +0700

    add tailwind

--- react/personal-notes-starter/src/index.js
+++ react/personal-notes-starter/src/index.js
@@ -2,7 +2,28 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 // import style
-import './styles/style.css';
+import './styles/index.css';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<h1>Hello, World!</h1>);
+root.render(
+  <div className='bg-gradient-to-r from-violet-900 to-fuchsia-700 h-screen w-screen'>
+    <div className='flex flex-row p-5 text-white'>
+      {/* content */}
+      <div className='basis-[75%]'>
+        <h1>Personal Notes</h1>
+        <div className='flex'>
+          <select className='bg-transparent'>
+            <option value='done'>done</option>
+            <option value='undone'>undone</option>
+          </select>
+          <input type='text' className='w-full' />
+          <span>icon</span>
+        </div>
+      </div>
+      {/* sidebar */}
+      <div className='basis-[25%]'>
+        <h1>Calendar</h1>
+      </div>
+    </div>
+  </div>
+);
