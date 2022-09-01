94322ecb5b1b64abe5750d381571ed6e51de470c
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-09-01 23:00:07 +0700

    lint

--- react/personal-notes-starter/src/components/Button.js
+++ react/personal-notes-starter/src/components/Button.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 function Button({ onClickEventHandler, value }) {
   return (
     <button
-      className='rounded bg-sky-500 px-2 py-1'
-      type='button'
+      className="rounded bg-sky-500 px-2 py-1"
+      type="button"
       onClick={onClickEventHandler}
     >
       {value}
--- react/personal-notes-starter/src/components/NoteItem.js
+++ react/personal-notes-starter/src/components/NoteItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 function NoteItem({
   id,
@@ -12,25 +12,27 @@ function NoteItem({
   return (
     <div
       key={id}
-      className='flex flex-col justify-between gap-2 rounded-lg bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100'
+      className="flex flex-col justify-between gap-2 rounded-lg bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100"
     >
-      <div className='p-3'>
-        <h1 className='font-bold text-lg'>{title}</h1>
-        <span className='text-sm text-yellow-200 my-1'>{new Date(createdAt).toLocaleString()}</span>
-        <p className='mt-2'>{body}</p>
+      <div className="p-3">
+        <h1 className="font-bold text-lg">{title}</h1>
+        <span className="text-sm text-yellow-200 my-1">
+          {new Date(createdAt).toLocaleString()}
+        </span>
+        <p className="mt-2">{body}</p>
       </div>
-      <div className='flex'>
+      <div className="flex">
         <button
-          className='flex-1 rounded-bl border border-gray-100 py-1 cursor-pointer text-red-500'
+          className="flex-1 rounded-bl border border-gray-100 py-1 cursor-pointer text-red-500"
           onClick={() => deleteNote(id)}
         >
           Delete
         </button>
         <button
-          className='flex-1 rounded-br border border-gray-100 py-1 cursor-pointer text-yellow-500'
+          className="flex-1 rounded-br border border-gray-100 py-1 cursor-pointer text-yellow-500"
           onClick={() => arsipNote(id)}
         >
-          {archived ? 'Pindahkan' : 'Arsipkan'}
+          {archived ? "Pindahkan" : "Arsipkan"}
         </button>
       </div>
     </div>
