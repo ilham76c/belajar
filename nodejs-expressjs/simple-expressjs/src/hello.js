11f2226c6d53c89d6afc9d6ee11c24326b3df2b2
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-12-03 21:07:35 +0700

    ExpressJS - Basic Routing

--- nodejs-expressjs/simple-expressjs/src/hello.js
+++ nodejs-expressjs/simple-expressjs/src/hello.js
@@ -2,6 +2,17 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+/**
+ * Jika membuat dua path yang sama, maka yang digunakan adalah yang pertama 
+ */
+// app.get('/', (req, res) => {
+//   res.send('Hello World! 2');
+// }); 
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on http://localhost:3000');
 });
