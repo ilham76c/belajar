528368706a82ff5e8b5460c6bd96e5be37d70890
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-12-05 07:44:22 +0700

    ExpressJS - Request

--- nodejs-expressjs/simple-expressjs/test/hello.test.js
+++ nodejs-expressjs/simple-expressjs/test/hello.test.js
@@ -3,12 +3,22 @@ import request from 'supertest';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 test('Hello World', async () => {
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
   const response = await request(app).get('/');
 
   expect(response.text).toBe('Hello World!');
+});
+
+test('Request', async () => {
+  app.get('/name', (req, res) => {
+    res.send(`Hello ${req.query.name}`);
+  });
+
+  const response = await request(app).get('/name').query({ name: 'ilham' });
+
+  expect(response.text).toBe('Hello ilham');
 });
