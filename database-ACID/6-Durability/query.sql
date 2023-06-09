01deffa12639df6135ae085a91e4a71b356f9a8d
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2023-06-09 23:57:41 +0700

    Database ACID - Durability

---
+++ database-ACID/6-Durability/query.sql
@@ -0,0 +1,22 @@
+-- 1. Transfer Gagal
+SELECT * FROM accounts;
+
+START TRANSACTION;
+
+SELECT * FROM accounts WHERE id in('ilham76c', 'bobay97') FOR UPDATE;
+
+UPDATE accounts SET balance = balance + 500000 WHERE id = 'bobay97';
+
+SELECT * FROM accounts;
+
+-- misal selanjutnya mysql-nya shutdown/mati
+
+-- lalu nyalakan lagi mysql-nya
+-- kemudian select data, data di table tidak akan berubah karena query di atas belum di commit
+SELECT * FROM accounts;
+
+UPDATE accounts SET balance = balance - 500000 WHERE id = 'ilham76c';
+
+SELECT * FROM accounts;
+
+COMMIT;
