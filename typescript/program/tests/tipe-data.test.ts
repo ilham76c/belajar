1855474252141364a432874633301b8faed7119f
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2023-07-18 23:36:48 +0700

    TypeScript: Tipe Data

---
+++ typescript/program/tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Data Type', function() {
+    it('should must declare', function() {
+        const name: string = "Moh. Ilham B.";
+        const balance : number = 6000000;
+        const isVip: boolean = true;
+
+        console.log(name);
+        console.log(balance);
+        console.log(isVip);
+    });
+});
