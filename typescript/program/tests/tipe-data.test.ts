fc9cface99aa98718123c40cc4ea0b3924c4afae
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2023-07-18 23:59:13 +0700

    TypeScript: Babel TypeSript

--- typescript/program/tests/tipe-data.test.ts
+++ typescript/program/tests/tipe-data.test.ts
@@ -8,4 +8,18 @@ describe('Data Type', function() {
         console.log(balance);
         console.log(isVip);
     });
+
+    it('should throw error', function() {
+        let name: string = "Moh. Ilham B.";
+        let balance : number = 6000000;
+        let isVip: boolean = true;
+
+        console.log(name);
+        console.log(balance);
+        console.log(isVip);
+
+        name = 1; // error
+        balance = "5000000"; // error
+        isVip = 7; // error
+    });
 });
