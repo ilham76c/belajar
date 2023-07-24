764ae31bd0ae5d152bcbee9fa8bd9f395c67a87d
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2023-07-24 23:23:09 +0700

    TypeScript: Function Parameter

--- typescript/program/tests/function.test.ts
+++ typescript/program/tests/function.test.ts
@@ -12,4 +12,39 @@ describe('Function', function() {
         
         printHello('Ilham');
     });
+
+    it('should support default value', function() {
+        function sayHello(name: string = 'Guest'): string {
+            return `Hello ${name}`;
+        }
+        
+        expect(sayHello('Ilham')).toBe('Hello Ilham');
+        expect(sayHello()).toBe('Hello Guest');
+    });
+
+    it('should support rest parameter', function() {
+        function sum(...values: number[]): number {
+            let total = 0;
+            for (const value of values) {
+                total += value;
+            }
+        
+            return total;
+        }
+        
+        expect(sum(1, 2, 3, 4, 5)).toBe(15);
+    });
+
+    it('should support optional parameter', function() {
+        function sayHello(firstName: string, lastName?: string): string {
+            if (lastName) {
+                return `Hello ${firstName} ${lastName}`;
+            }
+
+            return `Hello ${firstName}`;
+        }
+
+        expect(sayHello('Joy')).toBe('Hello Joy');
+        expect(sayHello('Mas', 'Ilham')).toBe('Hello Mas Ilham');
+    });
 });
