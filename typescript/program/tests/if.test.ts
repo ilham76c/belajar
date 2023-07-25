3a4a4116bda9da55ec0e67bba44a7e0985f55681
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2023-07-26 00:39:09 +0700

    TypeScript: Ternary Operator

--- typescript/program/tests/if.test.ts
+++ typescript/program/tests/if.test.ts
@@ -14,4 +14,19 @@ describe('IF Statement', function() {
         expect(examStatus(65)).toBe('Not Bad');
         expect(examStatus(40)).toBe('Try Again');
     });
+
+    it('should support ternary operator', function() {
+        const isOdd = (value: number) => { 
+            return value % 2 === 1 ? true : false 
+        };
+        const isEven = (value: number) => { 
+            return value % 2 === 0 ? true : false 
+        };
+
+        expect(isOdd(7)).toBe(true);
+        expect(isOdd(6)).toBe(false);
+
+        expect(isEven(10)).toBe(true);
+        expect(isEven(9)).toBe(false);
+    });
 });
